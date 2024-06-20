14afbbc1da13ace5eacf4e67145d2d2190b24325
Greg Cymbalski <greg.cymbalski@versity.com>
2024-06-20 15:00:39 -0700

    Use bundled NFPM config

--- package.sh
+++ package.sh
@@ -2,4 +2,4 @@
 
 export PATH="/go/bin:${PATH}"
 
-cd /scoutfs-fencing && nfpm pkg --packager rpm --conf /scoutfs-fencing/nfpm.yaml --target /package
+nfpm pkg --packager rpm --conf /scoutfs-fencing/nfpm.yaml --target /package
