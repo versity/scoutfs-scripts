43cad403d828343e302eb145485f67f67d1b68e2
Greg Cymbalski <greg.cymbalski@versity.com>
2024-06-20 15:34:57 -0700

    Working package generation for support scripts

--- build_package.sh
+++ build_package.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
+PULL="${PULL:-true}"
+GOLANG_IMAGE_NAME="${GOLANG_IMAGE_NAME:-golang}"
+GOLANG_IMAGE_VERSION="${GOLANG_IMAGE_VERSION:-latest}"
+
 export VERSION
-VERSION="$(git describe --all)"
+VERSION="$(git describe --tags)"
 
-docker build -t versity-scripts:latest .
+docker build --pull="${PULL}" \
+    --build-arg "GOLANG_IMAGE_NAME=${GOLANG_IMAGE_NAME}" \
+    --build-arg "GOLANG_IMAGE_VERSION=${GOLANG_IMAGE_VERSION}" \
+    -t "versity-scripts:${VERSION}" .
 
-docker run --rm -e "VERSION=${VERSION}" -v /package:${PWD}/package supportbuilder:latest
+docker run --rm -e "HOST_UID=${UID}" -e "VERSION=${VERSION}" -v "${PWD}/package:/package" "versity-scripts:${VERSION}"
